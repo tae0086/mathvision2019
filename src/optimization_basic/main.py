c30240d1ad740ab6a9f37b7fc55b6cef9001a8c6
DESKTOP-ETRI\TAE <tae0086@gmail.com>
2019-05-23 16:43:15 +0900

    hw12 작성중

--- src/optimization_basic/main.py
+++ src/optimization_basic/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits import mplot3d
-from sympy import Symbol, symbols, diff, pprint
+from sympy import symbols, solve_poly_system
 
 
 def f(x: np.ndarray, y: np.ndarray):
@@ -26,15 +26,17 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
 
-plt.show()
+# plt.show()
 
 # Q1-2
 # define function using symbol x and y
 x, y = symbols('x y')
 z = (x + y) * (x * y + x * y ** 2)
 z_gradient = [z.diff(x), z.diff(y)]  # gradient of z
-print(z_gradient)
+# print(z_gradient)
+# TODO: 검토 필요 - (0, 0)에서의 gradient가 (0, 0)
 print('({}, {})'.format(z_gradient[0].subs([(x, 0), (y, 0)]), z_gradient[1].subs([(x, 0), (y, 0)])))
 
 # Q1-3
-
+# find critical points
+print(solve_poly_system(z_gradient, [x, y]))  # [(0, -1), (0, 0), (3/8, -3/4), (1, -1)]
