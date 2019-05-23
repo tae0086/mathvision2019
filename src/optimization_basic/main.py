0db3cbf0fbb6c3703a68a30384ce838a638d8f04
DESKTOP-ETRI\TAE <tae0086@gmail.com>
2019-05-23 16:34:42 +0900

    hw12 작성중

---
+++ src/optimization_basic/main.py
@@ -0,0 +1,40 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits import mplot3d
+from sympy import Symbol, symbols, diff, pprint
+
+
+def f(x: np.ndarray, y: np.ndarray):
+    """Scalar fucntion for x and y."""
+    return (x + y) * (x * y + x * y ** 2)
+
+
+# Q1-1
+# define x, y, z as numpy array
+x = np.linspace(start=-2, stop=2, num=30)
+y = np.linspace(start=-2, stop=2, num=30)
+x, y = np.meshgrid(x, y)  # coordinate matrix from x and y vectors
+z = f(x, y)
+
+# plot 3d surface
+figure = plt.figure(figsize=(8, 6))
+ax = plt.axes(projection='3d')
+ax.plot_surface(x, y, z, cmap='viridis', antialiased=True)
+
+# label
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+plt.show()
+
+# Q1-2
+# define function using symbol x and y
+x, y = symbols('x y')
+z = (x + y) * (x * y + x * y ** 2)
+z_gradient = [z.diff(x), z.diff(y)]  # gradient of z
+print(z_gradient)
+print('({}, {})'.format(z_gradient[0].subs([(x, 0), (y, 0)]), z_gradient[1].subs([(x, 0), (y, 0)])))
+
+# Q1-3
+
